6ae6dd0e659af9c6c0ce8b8f1052935589eae61b
yigit <yboyar@gmail.com>
2025-01-24 23:57:27 -0800

    what if we weight by test counts? this complexity is too much but i'm curious :)

--- android-tests/device-tests/build.gradle.kts
+++ android-tests/device-tests/build.gradle.kts
@@ -1,8 +1,14 @@
 import java.io.FileOutputStream
+import java.util.PriorityQueue
 import java.util.jar.JarEntry
 import java.util.jar.JarFile
 import java.util.jar.JarOutputStream
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.capitalizeAsciiOnly
+import org.objectweb.asm.AnnotationVisitor
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes.ASM9
 
 plugins {
     alias(libs.plugins.android.library)
@@ -124,32 +130,99 @@ abstract class ShardTestsByClassTask : DefaultTask() {
 
     @get:OutputDirectory abstract val outputDirectory: DirectoryProperty
 
+    class Shard(val id: Int) : Comparable<Shard> {
+        private val entries: MutableList<JarEntry> = mutableListOf()
+        private var totalTests: Int = 0
+
+        fun addClass(entry: JarEntry, numberOfTests: Int) {
+            totalTests += numberOfTests
+            entries.add(entry)
+        }
+
+        fun entries(): List<JarEntry> = entries
+
+        override fun compareTo(other: Shard): Int {
+            val totalTestsCmp = totalTests.compareTo(other.totalTests)
+            if (totalTestsCmp != 0) return totalTestsCmp
+            return id.compareTo(other.id)
+        }
+    }
+
     @TaskAction
     fun shardTests() {
-        JarFile(testJar.get().asFile).use { inputJar ->
-            val (testClasses, otherClasses) =
-                inputJar.entries().asSequence().partition { it.realName.endsWith("Test.class") }
-            // Distribute test classes evenly across shards
-            val testClassesPerShard =
-                testClasses.chunked((testClasses.size + numberOfShards - 1) / numberOfShards)
-            testClassesPerShard.forEachIndexed { shardIndex, classes ->
-                JarOutputStream(
-                        FileOutputStream(outputDirectory.file("shard-$shardIndex.jar").get().asFile)
-                    )
-                    .use { jos ->
-                        // Add test classes for this shard
-                        classes.forEach { entry ->
-                            jos.putNextEntry(JarEntry(entry.name))
-                            inputJar.getInputStream(entry).use { it.copyTo(jos) }
-                        }
+        val jarFile = JarFile(testJar.get().asFile)
+        val entries = jarFile.entries().asSequence().toList()
 
-                        // Add all other files to each shard
-                        otherClasses.forEach { entry ->
-                            jos.putNextEntry(JarEntry(entry.name))
-                            inputJar.getInputStream(entry).use { it.copyTo(jos) }
+        // Distribute by weight
+        val priorityQueue = PriorityQueue<Shard>()
+        val shards = Array(numberOfShards) { Shard(id = it).also { priorityQueue.add(it) } }
+
+        // Sort classes by their test count and we'll then fill shards based on the next
+        // smallest shard. Not the most optimal solution wrt distribution but shall be good
+        // enough.
+        entries
+            .filter { it.name.endsWith("Test.class") }
+            .map { entry ->
+                val bytes = jarFile.getInputStream(entry).use { it.readBytes() }
+                val numberOfTests = countTestAnnotations(bytes)
+                entry to numberOfTests
+            }
+            .sortedByDescending { it.second }
+            .forEach { (entry, numberOfTests) ->
+                val shard = priorityQueue.poll()
+                shard.addClass(entry, numberOfTests)
+                priorityQueue.add(shard)
+            }
+
+        // Other files
+        val otherFiles = entries.filter { !it.name.endsWith("Test.class") }
+
+        // Create output JARs
+        shards.forEach { shard ->
+            JarOutputStream(
+                    FileOutputStream(outputDirectory.file("shard-${shard.id}.jar").get().asFile)
+                )
+                .use { jos ->
+                    shard.entries().forEach { entry ->
+                        jos.putNextEntry(JarEntry(entry.name))
+                        jarFile.getInputStream(entry).use { it.copyTo(jos) }
+                    }
+
+                    otherFiles.forEach { entry ->
+                        jos.putNextEntry(JarEntry(entry.name))
+                        jarFile.getInputStream(entry).use { it.copyTo(jos) }
+                    }
+                }
+        }
+
+        jarFile.close()
+    }
+
+    private fun countTestAnnotations(bytes: ByteArray): Int {
+        val cr = ClassReader(bytes)
+        var testCount = 0
+        cr.accept(
+            object : ClassVisitor(ASM9) {
+                override fun visitMethod(
+                    access: Int,
+                    name: String?,
+                    descriptor: String?,
+                    signature: String?,
+                    exceptions: Array<out String>?,
+                ): MethodVisitor {
+                    return object : MethodVisitor(ASM9) {
+                        override fun visitAnnotation(
+                            descriptor: String?,
+                            visible: Boolean,
+                        ): AnnotationVisitor? {
+                            if (descriptor == "Lorg/junit/jupiter/api/Test;") testCount++
+                            return null
                         }
                     }
-            }
-        }
+                }
+            },
+            ClassReader.SKIP_DEBUG or ClassReader.SKIP_FRAMES,
+        )
+        return testCount
     }
 }
